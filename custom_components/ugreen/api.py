4b2126d8ce30c5f8d126468620f2590d58e11f5d
Tom Bombadil <20954551+Tom-Bom-badil@users.noreply.github.com>
2025-07-18 10:24:59 +0200

    Add comment for deletion of _LOGGER.warning @usb_slot_list
    see https://github.com/Tom-Bom-badil/home-assistant_ugreen-nas/discussions/64

--- custom_components/ugreen/api.py
+++ custom_components/ugreen/api.py
@@ -799,7 +799,7 @@ class UgreenApiClient:
             return []
 
         usb_slot_list = data.get("data", {}).get("hardware", {}).get("usb", [])
-        if not usb_slot_list:
+        if not usb_slot_list: # _LOGGER.warning removed; list is not avail if no USB device is attached (=no error)
             return []
 
         entities: List[UgreenEntity] = []
